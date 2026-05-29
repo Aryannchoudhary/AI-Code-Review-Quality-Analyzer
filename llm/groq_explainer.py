8ef96033d82c20e5f33aebf31803f5e72793e2e9
Aryan <aryanchoudhary1762@gmail.com>
2026-05-29 23:52:35 +0530

    Fix-3

--- llm/groq_explainer.py
+++ llm/groq_explainer.py
@@ -1,8 +1,8 @@
 import requests
-import os
+import streamlit as st
 
 
-GROQ_API_KEY = os.secrets("GROQ_API_KEY")
+GROQ_API_KEY = st.secrets("GROQ_API_KEY")
 
 def explain_code_quality(code, prediction, features):
 
