c2010ca5e5a6062d54c172e4e177babce80b2524
Aryan <aryanchoudhary1762@gmail.com>
2026-05-30 14:13:07 +0530

    Update github fetcher

--- github_integration/github_fetcher.py
+++ github_integration/github_fetcher.py
@@ -1,66 +1,40 @@
 import requests
 
-
-
 def parse_repo_url(repo_url):
     parts = repo_url.rstrip("/").split("/")
     owner = parts[-2]
-    repo = parts[-1]
+    repo = parts[-1].replace(".git", "")
     return owner, repo
 
-
 def get_python_files(owner, repo):
-    """
-    Fetch all Python files from a public GitHub repository.
-    """
-
     api_url = f"https://api.github.com/repos/{owner}/{repo}/contents"
 
     python_files = []
 
     def traverse(url):
+        response = requests.get(url)
 
-    headers = {
-        "Accept": "application/vnd.github+json",
-        "User-Agent": "AI-Code-Analyzer"
-    }
+        if response.status_code != 200:
+            return
 
-    response = requests.get(url, headers=headers)
+        contents = response.json()
 
-    print("URL:", url)
-    print("Status Code:", response.status_code)
+        for item in contents:
+            if item["type"] == "file" and item["name"].endswith(".py"):
+                python_files.append(item["download_url"])
 
-    if response.status_code != 200:
-        print("Error:", response.text)
-        return
+            elif item["type"] == "dir":
+                traverse(item["url"])
 
-    contents = response.json()
+    traverse(api_url)
 
-    for item in contents:
-
-        print(item["name"], item["type"])
-
-        if item["type"] == "file" and item["name"].endswith(".py"):
-
-            print("FOUND PYTHON FILE:", item["name"])
-
-            python_files.append(
-                item["download_url"]
-            )
-
-        elif item["type"] == "dir":
-
-            traverse(
-                item["url"]
-            )
+    return python_files
 
 def get_file_content(download_url):
-
     response = requests.get(download_url)
 
     if response.status_code == 200:
         return response.text
 
     return None
-
 
